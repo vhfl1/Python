297786ace610a03f8499177a8e5da52ad52ad880
dpadptmxl3 <dpadptmxl3@gmail.com>
2023-01-03 12:23:18 +0900

    python

---
+++ Ch02/1.변수.py
@@ -0,0 +1,17 @@
+"""
+날짜 : 2023/01/02
+이름 : 윤사랑
+내용 : 파이썬 변수 실습하기
+"""
+
+var1 = 1
+var2 = 2
+
+var3 = var1 + var2
+print('var3: ', var3)
+
+var1 = 10
+var2 = 20
+
+var3 = var1 + var2
+print('var3 : ', var3)
